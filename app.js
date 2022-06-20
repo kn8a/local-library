576e275bbed332a422f29ca0997cd6136ef80cd5
kn8a <88045655+kn8a@users.noreply.github.com>
2022-06-20 12:27:53 +0700

    create models

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var mongoose = require('mongoose')
+var mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ryopv.mongodb.net/local_library?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
